55ab773e1a82a66b6336491b2e30e541898a942f
leticiasmx <sammyaleticia1234@gmail.com>
2025-11-10 21:31:35 -0300

    Create Node class for binary tree structure

---
+++ Node.java
@@ -0,0 +1,14 @@
+package arvorebinaria;
+
+public class Node {
+    int valor;
+    Node esquerda;
+    Node direita;
+    
+    public Node(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+}
+
